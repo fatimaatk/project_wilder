0148adfa3314383d5a2202e49a153859e33886c1
Thomas F <thomasfran1935@gmail.com>
2021-09-29 12:24:09 +0200

    Initial commit of prod branch

---
+++ contact.js
@@ -0,0 +1,4 @@
+document.getElementById("sendButton").addEventListener("click", function() {
+    const nom = document.getElementById("name");
+    alert(`Merci ${nom.value} pour votre message`);
+  });
