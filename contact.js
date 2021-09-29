36d65d55191b6cf22c0fbdf5a6094bd4e9dc6708
Thomas F <thomasfran1935@gmail.com>
2021-09-29 15:33:30 +0200

    Many edits about CSS and some JS tests

--- contact.js
+++ contact.js
@@ -1,4 +1,11 @@
 document.getElementById("sendButton").addEventListener("click", function() {
     const nom = document.getElementById("name");
+    const message = document.getElementById("message");
+    console.log("yes");
+    if (message.value === "Message" || message.value === "") {
+      alert("Merci de nous écrire votre message dans la zone prévue pour.");
+    }
+    else {
     alert(`Merci ${nom.value} pour votre message`);
+    }
   });
