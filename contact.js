5272c2fffd5d1f79a9c9b8da3d0c0f7fa9a687a4
Thomas F <thomasfran1935@gmail.com>
2021-10-01 10:52:10 +0200

    Tests about fun JS

--- contact.js
+++ contact.js
@@ -2,6 +2,12 @@ document.getElementById("sendButton").addEventListener("click", function() {
     const nom = document.getElementById("name");
     const message = document.getElementById("message");
     const email = document.getElementById("email");
+    if (nom.value === "plop" || nom.value === "Plop") {
+      const plopFooter = document.getElementById("plopFooter");
+      const plopBar = document.getElementById("plopBar");
+      plopFooter.style.background = "pink";
+      plopBar.style.background = "pink"; 
+    }
     if (email.value === "") {
       alert("Merci de renseigner une adresse e-mail.");
       console.log("Invalid email");
--- jenny.js
+++ jenny.js
@@ -7,3 +7,13 @@ darkMode = () => {
         checkContent.innerHTML = "Dark Mode";
     }
 }
+function uppercase(str) {
+
+let arrayStr = str.split(' ');
+
+for (let i = 0; i < arrayStr.length; i++) {
+    console.log(arrayStr[i]);
+    arrayStr[i] = arrayStr[i].charAt(0).toUpperCase() + arrayStr[i].substring(1);
+}
+return arrayStr.join(' ');
+}
